9119f485a22e172d7b939055c679a38113a69795
hussein <yuosef234t5y@gmail.com>
2026-05-23 18:43:29 +0300

    Initial server commit

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", // replace with your real password
-  database: "Scout",
-});
+require("dotenv").config();
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
 db.connect((err) => {
-    if (err) {
-      console.error('❌ MySQL connection error:', err);
-    } else {
-      console.log('✅ Connected to MySQL database!');
-    }
-  });
-module.exports = db;  
+  if (err) {
+    console.error("❌ MySQL connection error:", err);
+  } else {
+    console.log("✅ Connected to MySQL database!");
+  }
+});
+
+module.exports = db;
