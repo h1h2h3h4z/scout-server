653c8d483e8bd4c2893f474ce1d642e18614cdc0
hussein <yuosef234t5y@gmail.com>
2026-05-29 11:32:11 +0300

    last

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const db = mysql.createConnection(process.env.DATABASE_URL);
-//new
+console.log(process.env.DATABASE_URL);
+
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection error:", err);
