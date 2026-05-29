abb81c3aef8c3b15a7bfcbbc55ee61f09da79f0c
hussein <yuosef234t5y@gmail.com>
2026-05-29 11:23:57 +0300

    update database config

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 const db = mysql.createConnection(process.env.DATABASE_URL);
-
+//new
 db.connect((err) => {
   if (err) {
     console.error("❌ MySQL connection error:", err);
